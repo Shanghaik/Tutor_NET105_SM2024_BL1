49f87f877c33ebc43982b1904fb8ca31b488681c
shanghaik168@gmail.com <Shanghaik1998>
2024-05-28 13:08:09 +0700

    api first

---
+++ AppAPI/Controllers/MathController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MathController : ControllerBase
+    {
+        [HttpGet("get-leopad")]
+        public ActionResult<string> LuckyDraw(int betnumber, int betmoney)
+        {
+            Random r = new Random();
+            int result = r.Next(100000, 999999);
+            if ((result - betnumber) % 100 == 0)
+            {
+                return Ok($"Bạn rất may mắn, đã trúng {betmoney * 75}");
+            }
+            else return BadRequest("Bạn đừng đặt cược bản thân mình vào những trò may rủi, kết quả là " + result);
+        }
+        [HttpGet("solve-equation")]
+        public ActionResult<string> EquationSolve(int a, int b)
+        {
+            if (a == 0)
+            {
+                if (b == 0) return Ok("There is always a solution");
+                else return Ok("No solution");
+            }
+            else
+            {
+                if (b == 0) return Ok("There is only solution x = 0");
+                else return Ok($"The is only solution x = {b * -1.0 / a}");
+            }
+        }
+    }
+}
---
+++ AppAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]// bỏ đi được - route mặc định
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        // private readonly ILogger<WeatherForecastController> _logger;
+
+        //public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        [HttpGet("get-weather")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
